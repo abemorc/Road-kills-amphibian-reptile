6d63bc4ffbdca54f77c864bbcefc80dfc9a2b57d
abemorc <abemorc00@gmail.com>
2022-03-12 01:39:38 -0600

    Update

--- R/New_variables_analysis2022/11_Geophis.R
+++ R/New_variables_analysis2022/11_Geophis.R
@@ -1,16 +1,4 @@
 
 
-# grupos deseados
-genero <- c("GENERO")
 
-list_genero <- subgrupos(genero)
-View(list_genero)
-
-list_genero1 <- func_df_ready(list_genero)
-View(list_genero1)
-
-list_genero2 <- func_df_ready4(list_genero1)
-View(list_genero2)
-df_geophis <- list_genero2[["Geophis"]]
-View(df_geophis)
 
--- R/temp.R
+++ R/temp.R
@@ -1181,3 +1181,23 @@ View()
 
 
 
+
+
+# g
+genero <- c("GENERO")
+
+list_genero <- subgrupos(genero)
+View(list_genero)
+
+list_genero1 <- func_df_ready(list_genero)
+View(list_genero1)
+
+list_genero2 <- func_df_ready4(list_genero1)
+View(list_genero2)
+df_geophis <- list_genero2[["Geophis"]]
+View(df_geophis)
+
+
+
+
+
